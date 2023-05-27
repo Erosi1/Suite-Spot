4849157ec8c5f1f8ebf6946d0873190d4106d279
Erosi1 <em52473@ubt-uni.net>
2023-05-27 20:04:09 +0200

    Login / Register and JWT authentication

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,10 +1,10 @@
 import express from 'express'
+import { register,login} from '../controllers/auth.js';
 
 const router = express.Router();
 
-router.get("/",(req,res)=>{ 
-    res.send("Hello this is auth endpointi")
-})
+router.post("/register",register);
+router.post("/login",login);
 
 
 
