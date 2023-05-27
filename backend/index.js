03ebd5ca3f4d98e361932f583bfa87922ccd036a
Erosi1 <em52473@ubt-uni.net>
2023-05-27 16:41:41 +0200

    backend and connection to MongoDB

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import authRoute from './routes/auth.js'
+import usersRoute from './routes/users.js'
+import hotelsRoute from './routes/hotels.js'
+import roomsRoute from './routes/rooms.js'
+
+const app = express ()
+dotenv.config()
+const connect= async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MongoDB")
+    }catch(error){
+        throw(error);
+    }
+}
+mongoose.connection.on("disconnected",()=>{
+    console.log("mongoDB disconnected");
+})
+//middlewares
+app.use('/auth',authRoute);
+app.use('/users',usersRoute);
+app.use('/hotels',hotelsRoute);
+app.use('/rooms',roomsRoute);
+
+
+
+
+
+app.listen(8800,()=> {
+    connect()
+    console.log("Connected to backend!")
+})
