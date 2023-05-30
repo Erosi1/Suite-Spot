d9f47de4324455e890b0ea6847719d09699ba04c
Erosi1 <em52473@ubt-uni.net>
2023-05-30 19:03:57 +0200

    login context api to keep track of user data

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -1,7 +1,10 @@
 import "./navbar.css";
 import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
 
 const Navbar = () => {
+  const { user} = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleSuiteSpotClick = () => {
@@ -14,10 +17,10 @@ const Navbar = () => {
         <span className="logo" onClick={handleSuiteSpotClick}>
           SuiteSpot
         </span>
-        <div className="navItems">
+        {user ? user.username : (<div className="navItems">
           <button className="navButton">Register</button>
           <button className="navButton">Login</button>
-        </div>
+        </div>)}
       </div>
     </div>
   );
